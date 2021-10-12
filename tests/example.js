54d4c89121817511ab3546607a333801c4b68374
Andrew <4ndrew.fowler@gmail.com>
2021-10-12 12:40:46 +0100

    Test skeleton

---
+++ tests/example.js
@@ -0,0 +1,14 @@
+import { Selector } from 'testcafe';
+import examplePage from "./model/example.page";
+
+fixture `Getting Started`
+    .page `http://devexpress.github.io/testcafe/example`;
+
+test('Check header text', async t => {
+    await t
+        .typeText(examplePage.nameInput, 'John Smith')
+        .click(examplePage.submitButton)
+
+        // Use the assertion to check if the actual header text is equal to the expected one
+        .expect(Selector(examplePage.articleHeader).innerText).eql('Thank you, John Smith!');
+});
